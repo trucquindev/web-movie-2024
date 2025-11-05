aae2c94977ed52459f1213d80b53bd6b4486cf55
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2025-11-05 21:58:37 +0700

    bugfix(layout): fix image

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,8 @@
 import { useAuth } from '@/context/AuthContext';
-import { Search, Bell } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SearchBar from '../search/searchBar';
+import { Bell } from 'lucide-react';
 
 export default function Header() {
   const nagivate = useNavigate();
@@ -35,21 +36,12 @@ export default function Header() {
             <h1 className="text-white font-bold text-lg leading-none">
               RoPhim
             </h1>
-            <p className="text-xs text-slate-400">Phim hay có tô</p>
+            <p className="text-xs text-slate-400">Phim hay cả rổ</p>
           </div>
         </div>
 
         {/* Search Bar */}
-        <div className="flex-1 max-w-md">
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-500" />
-            <input
-              type="text"
-              placeholder="Tìm kiếm phim, diễn viên"
-              className="w-full pl-10 pr-4 py-2 bg-slate-800/50 border border-slate-700 rounded-lg text-slate-200 placeholder-slate-500 focus:outline-none focus:border-amber-400/50"
-            />
-          </div>
-        </div>
+        <SearchBar/>
 
         {/* Navigation */}
         <nav className="hidden md:flex items-center gap-6 text-sm">
